6585a2c33d500a5d552f6e02cd1a27368085262e
Enikő Veszelka <encsooo@gmail.com>
2021-02-11 10:26:06 +0100

    fetch

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import {useSelector} from "react-redux";
-import {Redirect} from 'react-router-dom'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
 
 const Admin = () => {
-    const isAuthenticated = useSelector(state => state)
-    console.log("admin=>",isAuthenticated)
+  const isAuthenticated = useSelector((state) => state);
+  console.log("admin=>", isAuthenticated);
 
-    if(!isAuthenticated) return <Redirect to="/"/>
-    
-    return (
-        <>
-        {isAuthenticated ? <h2>true</h2> : <h2>false</h2>}
-        </>
-    )
-}
+  if (!isAuthenticated) return <Redirect to="/" />;
 
-export default Admin
+  const fetchData = async () => {
+    try {
+      const res = await fetch(
+        "https://spreadsheets.google.com/feeds/cells/1lzHqm7_1FDtEQLvTOA8N98OMol001ZC1v4H6ec4QFBI/od6/public/basic?alt=json"
+      );
+      const data = await res.json();
+      console.log(data);
+    } catch {
+      console.log("error");
+    }
+  };
+  fetchData();
+
+  return <>{isAuthenticated ? <h2>true</h2> : <h2>false</h2>}</>;
+};
+
+export default Admin;
--- src/components/NotFound404.jsx
+++ src/components/NotFound404.jsx
@@ -3,7 +3,10 @@ import React from "react";
 export default function NotFound404() {
   return (
     <div className="notfound">
-      <h1>Not found</h1>
+      <div className="notfound-container">
+        <h1 className="notfound-title">Not found</h1>
+        <button className="notfound-btn">Go back</button>
+      </div>
     </div>
   );
 }
