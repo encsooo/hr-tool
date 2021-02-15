884f238381cac047317689ded8451adecd52c7aa
Enikő Veszelka <encsooo@gmail.com>
2021-02-15 16:32:39 +0100

    start checkin component

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,24 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import {useSelector} from "react-redux"
+import { useSelector } from "react-redux";
 import Home from "../components/Home";
 import Login from "../components/Login";
 import Admin from "../components/Admin";
-import Employee from "../components/Employee"
-import EmployeeRegister from "./EmployeeRegister"
+import Checkin from '../components/Checkin';
 import NotFound404 from "../components/NotFound404";
 
 const Routes = () => {
-  const isAuthenticated = useSelector(state => state)
+  const isAuthenticated = useSelector((state) => state);
   return (
     <main>
       <Router>
         <Switch>
           <Home path="/" exact component={Home} />
           {!isAuthenticated && <Route path="/login" component={Login} />}
-           <Route path="/admin" component={Admin} />
-           <Route path="/employee" component={Employee} />
-           <Route path="/employeeRegister" component={EmployeeRegister}/>
+          <Route path="/admin" component={Admin} />
+          <Route path="/checkin" component={Checkin} />
           <Route component={NotFound404} />
         </Switch>
       </Router>
