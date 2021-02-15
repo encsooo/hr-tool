caa3d545f667f1d504e86216ae40a835a9f8a744
Ines <inesfcnogueira@gmail.com>
2021-02-15 14:55:26 +0100

    reorganized css

--- src/components/NotFound404.jsx
+++ src/components/NotFound404.jsx
@@ -5,12 +5,12 @@ export default function NotFound404(props) {
   return (
     <>
       <div
-        className="notfound-header"
+        className="header-container"
         style={{
           background: `linear-gradient(0deg, rgba(9,39,235,0.7) 0%, rgba(9,39,235,0.7) 100%), url(${metro})`,
         }}
       >
-        <h2 className="notfound-title">Nothing to see here....</h2>
+        <h2 className="header-title">Nothing to see here....</h2>
       </div>
       <div className="notfound-main">
         <button className="notfound-btn" onClick={() => props.history.goBack()}>
